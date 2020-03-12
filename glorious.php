718ff3de80236d39edcfd3be640d56a36eb4d8c1
bolusams <60818970+bolusams@users.noreply.github.com>
2020-03-12 13:38:09 +0100

    Add files via upload

---
+++ glorious.php
@@ -0,0 +1,24 @@
+<?php
+    
+    //form
+    /*
+    <p><h1>Login</h1></p>
+    <div>
+       <h1>Name:</h1> <input type="text" name="name" placeholder="enter name">
+       <br/>
+       <h1>Address:</h1> <input type="text" name="address" placeholder="enter addres">
+       <br/>
+       <h1>Email:</h1> <input type="email" name="email" placeholder="enter a valid email">
+       <br/>
+       <h1>Phone number:</h1> <input type="text" name="ph" placeholder="enter phone no">
+       <br/>
+       <input type= "submit" value= "submit">
+       
+        
+    </div>
+    </form>
+    
+
+    
+
+
